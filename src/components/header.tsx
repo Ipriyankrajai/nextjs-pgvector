6e6a1909b61a0e2570b4644ed489802cfe7e2cc1
Priyank Rajai <priyank73@hotmail.com>
2024-10-26 19:29:08 +0530

    feat: header

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="p-5 max-w-screen-xl mx-auto flex justify-between items-center">
+      <h1 className="text-2xl font-bold">
+        <span className="text-red-600">pg</span>
+        vector
+      </h1>
+    </div>
+  );
+}
