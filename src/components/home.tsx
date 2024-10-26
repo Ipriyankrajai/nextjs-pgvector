4d6800583c798e0a3aba0f07d393676e5811070c
Priyank Rajai <priyank73@hotmail.com>
2024-10-26 21:00:08 +0530

    refactor: home

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,11 @@
+import SearchContainer from "./search/search-container";
 import Wrapper from "./wrapper";
 
 export default function Home() {
-  return <Wrapper>Home</Wrapper>;
+  return (
+    <Wrapper>
+      <h1 className="text-2xl font-bold text-center my-5">Heros</h1>
+      <SearchContainer />
+    </Wrapper>
+  );
 }
