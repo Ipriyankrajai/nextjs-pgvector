2edd3adc6d57291077dab7fc10716de0be86747f
Priyank Rajai <priyank73@hotmail.com>
2024-10-27 00:46:59 +0530

    refactor: home page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -4,7 +4,6 @@ import Wrapper from "./wrapper";
 export default function Home() {
   return (
     <Wrapper>
-      <h1 className="text-2xl font-bold text-center my-5">Heros</h1>
       <SearchContainer />
     </Wrapper>
   );
