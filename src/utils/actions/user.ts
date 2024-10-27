5d3f131c3af22d453e628077605de99ce0df76f4
Priyank Rajai <priyank73@hotmail.com>
2024-10-27 16:46:41 +0530

    feat: adding searchUsersthroughEmbedding

--- src/utils/actions/user.ts
+++ src/utils/actions/user.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import prisma from "@/utils/prisma";
+import generateEmbedding from "../generate-embeddings";
 
 export async function getUsers({ query }: { query: string }) {
   return await prisma.user.findMany({
@@ -16,9 +17,36 @@ export async function getUsers({ query }: { query: string }) {
 export async function addUsers({
   user,
 }: {
-  user: { name: string; profession: string; description: string };
+  user: {
+    name: string;
+    category: string;
+    description: string;
+    founders: string;
+  };
 }) {
-  return await prisma.user.create({
-    data: user,
-  });
+  const embedding = await generateEmbedding(user.description);
+  return await prisma.$executeRaw`
+    INSERT INTO "User" (name, category, description, founders, embedding)
+    VALUES (${user.name}, ${user.category}, ${user.description}, ${user.founders}, ${embedding}::vector)
+    RETURNING *
+  `;
+}
+
+export async function searchUsersthroughEmbedding({
+  query,
+}: {
+  query: string;
+}) {
+  const embedding = await generateEmbedding(query);
+  return await prisma.$queryRaw`
+    SELECT
+      id,
+      name,
+      category,
+      founders,
+      description
+    FROM "User"
+    ORDER BY embedding <-> ${embedding}::vector
+    LIMIT 20
+  `;
 }
